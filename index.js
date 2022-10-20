aa5a1f77ad3a9e30ff966d37fb9f189c5babb7b6
Bright Peter <brightbassey2001@gmail.com>
2022-10-20 13:20:28 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+require("dotenv").config();
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is up on ${process.env.PORT}`);
+});
